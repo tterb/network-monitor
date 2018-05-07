410b3a8f1d16351e27bf2ce8e64186c5ddfe0937
tterb <bstevensondev@gmail.com>
2018-05-07 03:11:45 -0700

    Improve network downtime reporting

--- network_monitor.py
+++ network_monitor.py
@@ -3,8 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from time import gmtime, strftime
+from datetime import datetime
 from email.message import EmailMessage
 
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 def main():
   parser = argparse.ArgumentParser()
@@ -13,7 +15,6 @@ def main():
   parser.add_argument('-g', '--graph', action="store_true", help='displays a graph of data speeds for the last 30 days')
   parser.add_argument('-r', '--report', action="store_true", help='sends an email with a report of your network data speeds for the last 30 days')
   args = parser.parse_args()
-  network_status = True
   if args.config:
     # Set target data speeds
     target = config()
@@ -34,20 +35,31 @@ def main():
   # Get current data speeds
   try:
     current = test_speed()
-    if not network_status:
-      sendEmail('Your network is back online')
-    network_status = True
   except:
     current = (0,0)
-    network_status = False
-    sendEmail('Your network appears to be offline')
-    print('Your network appears to be offline')
 
+  with open('log.json','r') as f:
+    log = json.load(f)
+    
+  index = -1
+  keys = list(log.keys())
+  prev = keys[index]
+  if 0 in log[prev]:
+    while 0 in log[prev]:
+      index -= 1
+      prev = keys[index]
+    print(keys[index])
+    hr = datetime.strptime(keys[index].split(':')[1], "%H").strftime("%I:%M %p")
+    d = [int(i) for i in keys[index].split(':')[0].split('-')]
+    msg = "Your network is back online.\nWe've recorded that your network has been down since "+hr+", on "+months[d[1]]+" "+str(d[2])+"."
+    print(msg)
+    sendEmail(msg)
+    
   # Log current data speeds
-  log(current[0], current[1])
+  logger(current[0], current[1])
   status = ['download','upload']
   for j,i in enumerate(status):
-    print(str(i[0].capitalize())+': '+str(current[j])+' mbps')
+    print(str(i.capitalize())+': '+str(current[j])+' mbps')
 
   if args.graph:
     plt = create_graph()
@@ -64,8 +76,9 @@ def test_speed():
   speed = subprocess.check_output(['python', 'speedtest.py', '--simple']).decode()
   return tuple([float(re.findall('\d+\.\d+', i)[0]) for i in speed.splitlines()[1:]])
 
+
 def config():
-  if os.path.exists('./config.json'):
+  if os.path.exists('config.json'):
     conf = json.load(open('config.json'))
   else:
     conf = dict()
@@ -76,7 +89,7 @@ def config():
   return conf
 
 
-def log(down, up):
+def logger(down, up):
   if os.path.exists('log.json'):
     with open('log.json','r') as f:
       log = json.load(f)
